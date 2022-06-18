43371d7930fa53081a06f861bf407c489bb36e03
buildsmile <you@example.com>
2022-06-18 11:02:00 +0700

    layout flutter

---
+++ lib/profile.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+
+      // body: Container(
+      //   width: double.infinity,
+      //
+      //   child: ListView(
+      //     children: [
+      //       Container(
+      //         child: ListTile(
+      //           leading: Icon(Icons.contact_mail),
+      //           title: Text('title'),
+      //           subtitle: Text('subtitle'),
+      //           trailing: Text('trailing'),
+      //         ),
+      //       ),
+      //       Container(
+      //         child: ListTile(
+      //           leading: Text('leading'),
+      //           title: Text('title'),
+      //           subtitle: Text('subtitle'),
+      //           trailing: Text('trailing'),
+      //         ),
+      //       ),
+      //       Container(
+      //         child: ListTile(
+      //           leading: Text('leading'),
+      //           title: Text('title'),
+      //           subtitle: Text('subtitle'),
+      //           trailing: Text('trailing'),
+      //         ),
+      //       ),
+      //       Container(
+      //         child: ListTile(
+      //           leading: Text('leading'),
+      //           title: Text('title'),
+      //           subtitle: Text('subtitle'),
+      //           trailing: Text('trailing'),
+      //         ),
+      //       ),
+      //       Container(
+      //         child: ListTile(
+      //           leading: Text('leading'),
+      //           title: Text('title'),
+      //           subtitle: Text('subtitle'),
+      //           trailing: Text('trailing'),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      //   // child: Column(
+      //   //   mainAxisAlignment: MainAxisAlignment.start,
+      //   //   children: [
+      //   //     Expanded(
+      //   //       flex: 2,
+      //   //       child: Container(
+      //   //         width: 100,
+      //   //         color: Colors.blueAccent,
+      //   //       ),
+      //   //     ),
+      //   //     Expanded(
+      //   //       flex: 5,
+      //   //       child: Container(
+      //   //         width: 100,
+      //   //         color: Colors.red,
+      //   //       ),
+      //   //     ),
+      //   //     Expanded(
+      //   //       child: Container(
+      //   //         width: 100,
+      //   //         color: Colors.yellow,
+      //   //       ),
+      //   //     ),
+      //   //   ],
+      //   // ),
+      // ),
+    );
+  }
+}
